a2c0324a5b8197cf37d56c6abf22b385a177a339
ssheaks <sarah.sheaks@gmail.com>
2018-02-20 09:38:23 -0800

    complete bonus question filteredNames

--- filterNames.js
+++ filterNames.js
@@ -4,10 +4,12 @@
 // Return only names that begin with 'R'
 const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filteredNames = filter(myNames, function(name) {
-  // This is a "predicate function" - it's a function that only returns a boolean
-  return name[0] === 'R';
-});
+const filteredNames = filter(myNames, (name => name[0] === 'R'))
+
+// const filteredNames = filter(myNames, function(name) {
+//   // This is a "predicate function" - it's a function that only returns a boolean
+//   return name[0] === 'R';
+// });
 
 console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
@@ -22,3 +24,5 @@ function filter(arr, fn) {
   }
   return newArray;
 }
+
+
