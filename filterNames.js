2806a3fd3b695334b1fe74689ee63e3135056578
ssheaks <sarah.sheaks@gmail.com>
2018-02-20 09:32:12 -0800

    complete filterNames drill

---
+++ filterNames.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  let newArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
