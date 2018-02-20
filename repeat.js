1228743a5bca8e66e990201dd1dd87a3dc0bcd61
trentduncan <trent.dunc@gmail.com>
2018-02-20 09:24:34 -0800

    added repeat

---
+++ repeat.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function repeat(fn, n) {
+	for(let i = 0; i < n; i++) {
+		fn();
+	}
+}
+
+function hello() {
+	console.log('Hello World');
+}
+
+function goodbye() {
+	console.log('Goodbye World');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
